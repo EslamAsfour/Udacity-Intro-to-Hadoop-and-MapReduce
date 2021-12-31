ce88a4b9d797c56fe130445fa5fea115d3021ef8
EslamAsfour <40435240+EslamAsfour@users.noreply.github.com>
2021-12-31 02:24:00 +0200

    Create Top10.py

---
+++ Top10.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+"""
+Your mapper function should print out 10 lines containing longest posts, sorted in
+ascending order from shortest to longest.
+Please do not use global variables and do not change the "main" function.
+"""
+import sys
+import csv
+
+
+def mapper():
+    reader = csv.reader(sys.stdin, delimiter='\t')
+    writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+    #Start Your Code
+    allLines =[]
+    for line in reader:
+        allLines.append(line)
+    max = 0
+    stage = ""
+    added = []
+    for i in range(10):
+        max = 0
+        reader = csv.reader(sys.stdin, delimiter='\t')
+        for line in allLines:
+            #print(line not in added)
+            if(len(line[4]) > max and line not in added):
+                max = len(line[4])
+                stage = line
+        added.append(stage)
+    added.reverse()
+    for i in added:
+        
+        writer.writerow(i)
+
+
+
+test_text = """\"\"\t\"\"\t\"\"\t\"\"\t\"333\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"88888888\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"1\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"11111111111\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"1000000000\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"22\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"4444\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"666666\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"55555\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"999999999\"\t\"\"
+\"\"\t\"\"\t\"\"\t\"\"\t\"7777777\"\t\"\"
+"""
+
+# This function allows you to test the mapper with the provided test string
+def main():
+    import StringIO
+    sys.stdin = StringIO.StringIO(test_text)
+    mapper()
+    sys.stdin = sys.__stdin__
+
+main()
